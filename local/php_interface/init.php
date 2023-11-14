f3ed0418088a79b4359971a91d856f78a1831e5f
Aleksandr Miasniankin <jivoy1057@mail.ru>
2023-11-14 16:44:26 +0300

    подключил iblock

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,11 +1,2 @@
 <?php
-// Вырезаем type="text/javascript ==============================================================================
-AddEventHandler("main", "OnEndBufferContent", "removeType");
-function removeType(&$content)
-{
-    $content = replace_output($content);
-}
-function replace_output($d)
-{
-    return str_replace(' type="text/javascript"', "", $d);
-}
+\Bitrix\Main\Loader::includeModule('iblock');

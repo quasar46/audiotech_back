a51e3e56e10fe526a308470dd2c0e4d2a6fa6b19
Aleksandr Miasniankin <jivoy1057@mail.ru>
2023-11-14 18:06:36 +0300

    вывод верстки до и после блока вызова компонента новости

--- local/templates/audiotech/components/bitrix/news/news/news.php
+++ local/templates/audiotech/components/bitrix/news/news/news.php
@@ -1,7 +1,6 @@
 <?php
-if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)
-{
-	die();
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
+    die();
 }
 /** @var array $arParams */
 /** @var array $arResult */
@@ -14,58 +13,102 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)
 /** @var string $templateFolder */
 /** @var string $componentPath */
 /** @var CBitrixComponent $component */
-$this->setFrameMode(true);
+$this->setFrameMode(true); ?>
 
-$APPLICATION->IncludeComponent(
-	"bitrix:news.list",
-	"news",
-	[
-		"IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
-		"IBLOCK_ID" => $arParams["IBLOCK_ID"],
-		"NEWS_COUNT" => $arParams["NEWS_COUNT"],
-		"SORT_BY1" => $arParams["SORT_BY1"],
-		"SORT_ORDER1" => $arParams["SORT_ORDER1"],
-		"SORT_BY2" => $arParams["SORT_BY2"],
-		"SORT_ORDER2" => $arParams["SORT_ORDER2"],
-		"FIELD_CODE" => $arParams["LIST_FIELD_CODE"],
-		"PROPERTY_CODE" => $arParams["LIST_PROPERTY_CODE"],
-		"DETAIL_URL" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["detail"],
-		"SECTION_URL" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["section"],
-		"IBLOCK_URL" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["news"],
-		"SET_TITLE" => $arParams["SET_TITLE"],
-		"SET_LAST_MODIFIED" => $arParams["SET_LAST_MODIFIED"],
-		"MESSAGE_404" => $arParams["MESSAGE_404"],
-		"SET_STATUS_404" => $arParams["SET_STATUS_404"],
-		"SHOW_404" => $arParams["SHOW_404"],
-		"FILE_404" => $arParams["FILE_404"],
-		"INCLUDE_IBLOCK_INTO_CHAIN" => $arParams["INCLUDE_IBLOCK_INTO_CHAIN"],
-		"CACHE_TYPE" => $arParams["CACHE_TYPE"],
-		"CACHE_TIME" => $arParams["CACHE_TIME"],
-		"CACHE_FILTER" => $arParams["CACHE_FILTER"],
-		"CACHE_GROUPS" => $arParams["CACHE_GROUPS"],
-		"DISPLAY_TOP_PAGER" => $arParams["DISPLAY_TOP_PAGER"],
-		"DISPLAY_BOTTOM_PAGER" => $arParams["DISPLAY_BOTTOM_PAGER"],
-		"PAGER_TITLE" => $arParams["PAGER_TITLE"],
-		"PAGER_TEMPLATE" => $arParams["PAGER_TEMPLATE"],
-		"PAGER_SHOW_ALWAYS" => $arParams["PAGER_SHOW_ALWAYS"],
-		"PAGER_DESC_NUMBERING" => $arParams["PAGER_DESC_NUMBERING"],
-		"PAGER_DESC_NUMBERING_CACHE_TIME" => $arParams["PAGER_DESC_NUMBERING_CACHE_TIME"],
-		"PAGER_SHOW_ALL" => $arParams["PAGER_SHOW_ALL"],
-		"PAGER_BASE_LINK_ENABLE" => $arParams["PAGER_BASE_LINK_ENABLE"],
-		"PAGER_BASE_LINK" => $arParams["PAGER_BASE_LINK"],
-		"PAGER_PARAMS_NAME" => $arParams["PAGER_PARAMS_NAME"],
-		"DISPLAY_DATE" => $arParams["DISPLAY_DATE"],
-		"DISPLAY_NAME" => "Y",
-		"DISPLAY_PICTURE" => $arParams["DISPLAY_PICTURE"],
-		"DISPLAY_PREVIEW_TEXT" => $arParams["DISPLAY_PREVIEW_TEXT"],
-		"PREVIEW_TRUNCATE_LEN" => $arParams["PREVIEW_TRUNCATE_LEN"],
-		"ACTIVE_DATE_FORMAT" => $arParams["LIST_ACTIVE_DATE_FORMAT"],
-		"USE_PERMISSIONS" => $arParams["USE_PERMISSIONS"],
-		"GROUP_PERMISSIONS" => $arParams["GROUP_PERMISSIONS"],
-		"FILTER_NAME" => $arParams["FILTER_NAME"],
-		"HIDE_LINK_WHEN_NO_DETAIL" => $arParams["HIDE_LINK_WHEN_NO_DETAIL"],
-		"CHECK_DATES" => $arParams["CHECK_DATES"],
-	],
-	$component,
-	['HIDE_ICONS' => 'Y']
-);
+<div class="about">
+    <div class="_container">
+        <h1 class="title-page">О нас</h1>
+    </div>
+    <div class="_container _container--mode">
+        <div class="tabs__nav">
+            <a class="tabs__btn" href="/about/company">Компания</a> <a class="tabs__btn" href="/about/specialists">Специалисты</a>
+            <a class="tabs__btn active" href="/about/news">Новости</a> <a class="tabs__btn" href="/about/licenses">Лицензии</a>
+        </div>
+        <div class="tabs__content">
+            <div class="tabs__pane show">
+                <div class="tabs__pane-wrap">
+                    <?php $APPLICATION->IncludeComponent(
+                        'audiotech:section.list',
+                        '',
+                    ) ?>
+                    <div class="news">
+                        <div class="subscribe">
+                            <h2 class="aside-block__title">Подписаться на рассылку</h2>
+                            <div class="input__overlay">
+                                <input class="input-default" type="email" placeholder="Электронная почта">
+                            </div>
+                            <p class="aside-block__agree">
+                                Подписываясь, вы даете согласие на <a href="">обработку персональных данных</a>
+                            </p>
+                            <button class="btn btn--red btn--l btn--icn" type="submit">
+                                Подписаться
+                            </button>
+                        </div>
+                        <?php $APPLICATION->IncludeComponent(
+                            "bitrix:news.list",
+                            "news",
+                            [
+                                "IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
+                                "IBLOCK_ID" => $arParams["IBLOCK_ID"],
+                                "NEWS_COUNT" => $arParams["NEWS_COUNT"],
+                                "SORT_BY1" => $arParams["SORT_BY1"],
+                                "SORT_ORDER1" => $arParams["SORT_ORDER1"],
+                                "SORT_BY2" => $arParams["SORT_BY2"],
+                                "SORT_ORDER2" => $arParams["SORT_ORDER2"],
+                                "FIELD_CODE" => $arParams["LIST_FIELD_CODE"],
+                                "PROPERTY_CODE" => $arParams["LIST_PROPERTY_CODE"],
+                                "DETAIL_URL" => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["detail"],
+                                "SECTION_URL" => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["section"],
+                                "IBLOCK_URL" => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["news"],
+                                "SET_TITLE" => $arParams["SET_TITLE"],
+                                "SET_LAST_MODIFIED" => $arParams["SET_LAST_MODIFIED"],
+                                "MESSAGE_404" => $arParams["MESSAGE_404"],
+                                "SET_STATUS_404" => $arParams["SET_STATUS_404"],
+                                "SHOW_404" => $arParams["SHOW_404"],
+                                "FILE_404" => $arParams["FILE_404"],
+                                "INCLUDE_IBLOCK_INTO_CHAIN" => $arParams["INCLUDE_IBLOCK_INTO_CHAIN"],
+                                "CACHE_TYPE" => $arParams["CACHE_TYPE"],
+                                "CACHE_TIME" => $arParams["CACHE_TIME"],
+                                "CACHE_FILTER" => $arParams["CACHE_FILTER"],
+                                "CACHE_GROUPS" => $arParams["CACHE_GROUPS"],
+                                "DISPLAY_TOP_PAGER" => $arParams["DISPLAY_TOP_PAGER"],
+                                "DISPLAY_BOTTOM_PAGER" => $arParams["DISPLAY_BOTTOM_PAGER"],
+                                "PAGER_TITLE" => $arParams["PAGER_TITLE"],
+                                "PAGER_TEMPLATE" => $arParams["PAGER_TEMPLATE"],
+                                "PAGER_SHOW_ALWAYS" => $arParams["PAGER_SHOW_ALWAYS"],
+                                "PAGER_DESC_NUMBERING" => $arParams["PAGER_DESC_NUMBERING"],
+                                "PAGER_DESC_NUMBERING_CACHE_TIME" => $arParams["PAGER_DESC_NUMBERING_CACHE_TIME"],
+                                "PAGER_SHOW_ALL" => $arParams["PAGER_SHOW_ALL"],
+                                "PAGER_BASE_LINK_ENABLE" => $arParams["PAGER_BASE_LINK_ENABLE"],
+                                "PAGER_BASE_LINK" => $arParams["PAGER_BASE_LINK"],
+                                "PAGER_PARAMS_NAME" => $arParams["PAGER_PARAMS_NAME"],
+                                "DISPLAY_DATE" => $arParams["DISPLAY_DATE"],
+                                "DISPLAY_NAME" => "Y",
+                                "DISPLAY_PICTURE" => $arParams["DISPLAY_PICTURE"],
+                                "DISPLAY_PREVIEW_TEXT" => $arParams["DISPLAY_PREVIEW_TEXT"],
+                                "PREVIEW_TRUNCATE_LEN" => $arParams["PREVIEW_TRUNCATE_LEN"],
+                                "ACTIVE_DATE_FORMAT" => $arParams["LIST_ACTIVE_DATE_FORMAT"],
+                                "USE_PERMISSIONS" => $arParams["USE_PERMISSIONS"],
+                                "GROUP_PERMISSIONS" => $arParams["GROUP_PERMISSIONS"],
+                                "FILTER_NAME" => $arParams["FILTER_NAME"],
+                                "HIDE_LINK_WHEN_NO_DETAIL" => $arParams["HIDE_LINK_WHEN_NO_DETAIL"],
+                                "CHECK_DATES" => $arParams["CHECK_DATES"],
+                            ],
+                            $component,
+                            ['HIDE_ICONS' => 'Y']
+                        ); ?>
+                    </div>
+                    <div class="pagination">
+                        <div class="pagination__list">
+                            <a class="pagination__prev" href=""> </a><a class="pagination__page active" href="">1</a><a
+                                    class="pagination__page" href="">2</a><a class="pagination__page" href="">...</a><a
+                                    class="pagination__page" href="">19</a><a class="pagination__next" href=""> </a>
+                        </div>
+                        <button class="btn btn--grey btn--m">Показать ещё &nbsp;<span id="cur-page">15</span>&nbsp;из&nbsp;
+                            <span id="total-pages">100</span></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
